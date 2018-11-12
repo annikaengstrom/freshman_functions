f9bc7f26ca7de063158d2d5d99b2723361d4c515
Richard Qiu <rqiu@college.harvard.edu>
2018-11-12 14:19:37 -0500

    Added library calls

--- upshot_downloads/app.R
+++ upshot_downloads/app.R
@@ -8,6 +8,8 @@
 #
 
 library(shiny)
+library(tidyverse)
+library()
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -36,12 +38,8 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+     
+     
    })
 }
 
