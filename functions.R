438e56343b2714c33b565a91123eec8c5c0d664a
Richard Qiu <rqiu@college.harvard.edu>
2018-11-12 14:06:31 -0500

    Initial commit

---
+++ functions.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+get_filename <- function(race) {
+  race <- str_to_lower(race)
+  paste0("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-", race, ".csv")
+}
+
+get_tibble <- function(path) {
+  download.file(url = path,
+                destfile = "temp.csv",
+                quiet = TRUE,
+                mode = "wb")
+  raw <- read_csv("temp.csv")
+  file.remove("temp.csv")
+  raw
+}
